09ee008148c4106e6a341fb5f9547dd04e83cb4d
David Dejaeghere <david.dejaeghere@tarpit.be>
2023-09-22 16:41:29 +0200

    adding dawn suppor

---
+++ keyboards/mechstudio/dawn/keymaps/vial/config.h
@@ -0,0 +1,9 @@
+* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0x91, 0x81, 0xC7, 0x34, 0xC4, 0x45, 0x24, 0x79}
+
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 3 }
+
+#define VIAL_UNLOCK_COMBO_COLS { 0, 13 }
